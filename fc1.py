dd22941a28f208b665574ea20276dd10128974f1
AmirYousofAbdi <56346544+AmirYousofAbdi@users.noreply.github.com>
2020-10-28 09:18:32 +0330

    Update fc1.py

--- fc1.py
+++ fc1.py
@@ -1,9 +1,4 @@
-'''
-import face_recognition as fc
-from cv2 import imshow as i
-image = fc.load_image_file("D:\\PROGRAMMING\\images\\pic4.jpg")
-file_locations = fc.face_locations(image)
-'''
+
 
 import face_recognition
 known_image = face_recognition.load_image_file("D:\\PROGRAMMING\\images\\pic4.jpg")
