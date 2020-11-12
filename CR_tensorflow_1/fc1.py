88c5de0fa7d45ed9beaeab2181d16993c061e756
unknown <amirabdi71@outlook.com>
2020-11-12 14:06:25 +0330

    tensorflow v.1

---
+++ CR_tensorflow_1/fc1.py
@@ -0,0 +1,70 @@
+
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from keras.preprocessing.image import load_img,img_to_array
+import os
+import matplotlib.pyplot as plt
+import cv2
+
+x_train = []
+y_train = []
+x_test = []
+y_test = []
+x_predicted = []
+global celeb
+celeb = ["anne_hathaway","arnold_schwarzenegger","ben_afflek","dwayne_johnson","elton_john","jerry_seinfeld","kate_beckinsale",
+        "keanu_reeves","lauren_cohan","madonna","mindy_kaling","simon_pegg","sofia_vergara","will_smith"] 
+
+
+
+def f(name):
+    for i in range(len(celeb)):
+        if celeb[i] == name:
+            return i+1
+
+
+
+
+for folder in os.listdir("E:\\P\\PYTHON\\CR\\images\\train"):
+    for file in os.listdir(f"E:\\P\\PYTHON\\CR\\images\\train\\{folder}"):
+        
+        #image = cv2.imread(f"E:\\P\\PYTHON\\CR\\images\\train\\{folder}\\{file}" ,cv2.IMREAD_GRAYSCALE)
+        img = load_img(f"E:\\P\\PYTHON\\CR\\images\\train\\{folder}\\{file}")
+        image = img_to_array(img)
+        x_train.append(image) 
+        
+        y_train.append(f(folder))
+
+
+
+for folder in os.listdir("E:\\P\\PYTHON\\CR\\images\\test"):
+    for file in os.listdir(f"E:\\P\\PYTHON\\CR\\images\\test\\{folder}"):
+        
+        #image = cv2.imread(f"E:\\P\\PYTHON\\CR\\images\\test\\{folder}\\{file}" ,cv2.IMREAD_GRAYSCALE)
+        img = load_img(f"E:\\P\\PYTHON\\CR\\images\\test\\{folder}\\{file}")
+        image = img_to_array(img)
+        
+        x_test.append(image)
+        y_test.append(f(folder))
+
+
+print(x_train)
+print("---------------------------")
+print(y_train)
+
+model = Sequential()
+model.add(Flatten())
+
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(len(celeb), (3, 3), activation='relu'))
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=10)
+
+y = model.predict(x_test)
+
+
